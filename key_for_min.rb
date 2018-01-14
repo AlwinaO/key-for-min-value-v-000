7b37586cce989dc00cb9137dc02a1099ea3cb986
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-01-14 20:09:26 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,19 +8,15 @@ def key_for_min_value(hash)
   smallest_value = nil
   hash.each do |k,v| #1. iterate over hash
 
-    if smallest_value == nil #2. compare key/value
-      smallest_value = v
-      key_of_smallest_value = k
-    else
-      if v < smallest_value #3. capture outcome
+      if smallest_value == nil #2. compare key/value
         smallest_value = v
         key_of_smallest_value = k
+      else
+            if v < smallest_value #3. capture outcome
+              smallest_value = v
+              key_of_smallest_value = k
+            end
       end
     end
-  end
   key_of_smallest_value #4. go back and iterate over hash
-end
-
-
-
 end
